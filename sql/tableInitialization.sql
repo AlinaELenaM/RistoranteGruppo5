0d6207cc2df8e9ff26ec98365ffd714dcd47b520
Alina Mustata <mustata_alina_elena@hotmail.com>
2023-01-16 09:50:49 +0100

    Feat: added tableInitialization.sql file

---
+++ sql/tableInitialization.sql
@@ -0,0 +1,2 @@
+CREATE TABLE IF NOT EXISTS newdb.cliente ( cliente_id INT auto_increment NOT NULL, cognome varchar(100) NULL, gusti varchar(15) NULL, CONSTRAINT cliente_pk PRIMARY KEY (cliente_id);
+INSERT INTO newdb.cliente (cognome, gusti) VALUES ('cliente.getCognome()', 'cliente.getGusti()');
