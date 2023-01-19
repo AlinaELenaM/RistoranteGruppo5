b9f35059514b8ec61c778d0671dbf3daefbf3bae
Marco Abbondanza <107813503+Marco9811@users.noreply.github.com>
2023-01-19 16:31:15 +0100

    AddMenuService

--- src/Ristorante/SqlService/MenuService.java
+++ src/Ristorante/SqlService/MenuService.java
@@ -1,10 +1,13 @@
 package Ristorante.SqlService;
 
-import java.sql.*;
+import Ristorante.consumatore.Cliente;
+import Ristorante.enums.MenuTypeEnum;
+import Ristorante.foodAndDrink.Menu;
 
-/**
- * The class Menu service that manages the Menu Table on the database
- */
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MenuService {
 
     /**
@@ -32,10 +35,8 @@ public class MenuService {
         String queryCreateMenu = ""
                 + "CREA TABELLA newdb.menu ( "
                 + "menu_id INT NOT NULL, "
-                + " portata varchar(25) NULL, "
                 + " nome varchar(100) NULL, "
-                + " `tipo` varchar(25) NULL, "
-                + " coperto varchar(100) NULL "
+                + " `tipo` varchar(25) NULL "
                 + ") "
                 + "MOTORE=InnoDB "
                 + "SET CARATTERI PREDEFINITO=utf8mb4 "
@@ -44,5 +45,84 @@ public class MenuService {
         statement.executeUpdate(queryCreateMenu);
         statement.close();
         System.out.println("Eseguito con successo!");
+    }
+    /*
+     insert menu in the menu table
+     */
+    public void insertMenuIntoTable(Menu menu) throws SQLException {
+        Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+        Statement statement = connection.createStatement();
+
+        String insertQuery =
+                """ 
+                 INSERT INTO newdb.menu (name, type)
+                 VALUES ('""" + menu.getName() + "', '" + menu.getType() + "');";
+
+        statement.executeUpdate(insertQuery);
+        statement.close();
+        System.out.println("Menu inserito!");
+    }
+    /*
+    update the menu in the menu table
+    */
+    public void updateMenuDetails(Menu menu, String newName, MenuTypeEnum newType) throws SQLException {
+        Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+        Statement statement = connection.createStatement();
+
+        String updateMenuQuery =  ""
+                + "UPDATE newdb.menu "
+                + "SET name='" + newName + " ', type='" + newType + "' "
+                + "WHERE menu_id =" + menu.getMenuId() + " ;";
+
+        statement.executeUpdate(updateMenuQuery);
+        statement.close();
+        System.out.println("Menu modificato!");
+    }
+    /*
+    delete the menu in the menu table
+     */
+    public void deleteMenu(Menu menu) throws SQLException{
+        Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+        Statement statement = connection.createStatement();
+
+        String deleteMenuQuery = "DELETE FROM menu WHERE menu_id= " + menu.getMenuId() + ";";
+
+        statement.executeUpdate(deleteMenuQuery);
+        statement.close();
+        System.out.println("Menu cancellato!");
+    }
+
+    /*
+    the list of all menu in database
+     */
+    public List<Menu> getAllMenu() throws SQLException {
+        Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+        Statement statement = connection.createStatement();
+
+        String selectQuery = """
+                SELECT * from newdb.menu;
+                """;
+
+        ResultSet resultSet = statement.executeQuery(selectQuery);
+        List<Menu> menu = new ArrayList<>();
+
+        while (resultSet.next()) {
+            menu.add(new Menu(resultSet.getInt("menu_id"),
+                    resultSet.getString("name"),
+                    MenuTypeEnum.valueOf(resultSet.getString("type"))));
+        }
+
+        statement.close();
+        return menu;
+    }
+/*
+print all menu in the table
+ */
+    public void printAllMenu() throws SQLException {
+
+        List<Menu> menu = getAllMenu();
+
+        System.out.println("Menu menu: ");
+        System.out.println(menu);
     }
 }
